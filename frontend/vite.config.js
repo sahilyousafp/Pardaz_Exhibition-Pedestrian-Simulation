f5a5c8036d8899538cc1776440150d9cd00ed45d
Sahil Yousaf <sahil.yousaf@students.iaac.net>
2026-05-17 20:31:59 +0200

    Added env variable to the vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,9 +16,18 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:8000',
-      '/uploads': 'http://localhost:8000',
-      '/heatmaps': 'http://localhost:8000',
+      '/api': {
+        target: process.env.VITE_BACKEND_HOST || 'http://localhost:8000',
+        changeOrigin: true
+      },
+      '/uploads': {
+        target: process.env.VITE_BACKEND_HOST || 'http://localhost:8000',
+        changeOrigin: true
+      },
+      '/heatmaps': {
+        target: process.env.VITE_BACKEND_HOST || 'http://localhost:8000',
+        changeOrigin: true
+      }
     },
   },
 })
