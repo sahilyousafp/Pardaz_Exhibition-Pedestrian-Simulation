79e901b9adb7b2fc2755c0dcae9942b64ba195a1
Sahil Yousaf <sahil.yousaf@students.iaac.net>
2026-05-17 21:07:51 +0200

    Locally test works

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
   preview: {
     host: '0.0.0.0',
     port: 4173,
@@ -11,9 +15,5 @@ export default defineConfig({
       'localhost',
       '127.0.0.1'
     ]
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173
   },
 })
