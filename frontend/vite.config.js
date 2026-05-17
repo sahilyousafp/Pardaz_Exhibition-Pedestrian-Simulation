719184aa2049b5af5c9854d81a06f20d84a44b79
Sahil Yousaf <sahil.yousaf@students.iaac.net>
2026-05-17 20:21:32 +0200

    Updated the vite config with allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: [
+      'frontend-production-bd98.up.railway.app',
+      'localhost',
+      '127.0.0.1'
+    ]
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
